757ce57efab55fdc0a3bee2a49829d6a50dc922d
Vitor <vitormellocunha@gmail.com>
2026-03-15 20:27:36 -0300

    adicionando servico e style web

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,29 +1,31 @@
 import os
-
 from flask import Flask
 
 def create_app(test_config=None):
-    # Criar e configurar o app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-    
+
     if test_config is None:
-        # Carregar a configuração do arquivo config.py, se existir
         app.config.from_pyfile('config.py', silent=True)
-    
     else:
-        # Carregar a configuração de test_config se for passada
         app.config.from_mapping(test_config)
-        
-    # Garantir que a pasta instance exista
-    os.makedirs(app.instance_path, exist_ok=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
     
-    # Uma simples rota de teste para verificar se o app está funcionando
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import auth
+    app.register_blueprint(auth.bp)
     
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
